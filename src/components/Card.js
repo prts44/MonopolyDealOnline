c0bed00b5f384347ab5276c3452ed05d26762ede
z <ryan.ckit57@gmail.com>
2022-09-18 19:31:05 -0400

    Improve card appearance

--- src/components/Card.js
+++ src/components/Card.js
@@ -10,8 +10,11 @@ function Card(props) {
             </span>);
         } else if (props.card.type === "renta") {
 
-        } else if (props.card.type === "wildpropertya") {
-        
+        } else if (props.card.type === "wildproperty") {
+            return (<span>
+                <input disabled={props.disabled} className={style.radioButton} type="radio" value={props.card} name="hand" id={props.card.internalId} onClick={() => {props.callback(props.card)}}/>
+                <label className={style.card} htmlFor={props.card.internalId}><p style={{backgroundColor: props.card.colours[0], color: props.card.colours[0]}}>a</p><p style={{backgroundColor: props.card.colours[1], color: props.card.colours[1]}}>a</p>Wild Property<br/>Value: {props.card.value}</label>
+            </span>);
         } else if (props.card.type === "property") {
             let cardName = "N/A";
             if (props.card.name) {
@@ -20,7 +23,7 @@ function Card(props) {
             }
             return (<span>
                 <input disabled={props.disabled} className={style.radioButton} type="radio" value={props.card} name="hand" id={props.card.internalId} onClick={() => {props.callback(props.card)}}/>
-                <label className={style.card} htmlFor={props.card.internalId}>{cardName}<br/>Value: {props.card.value}<br/>{props.card.rent.map((r) => <p>{r}</p>)}</label>
+                <label className={style.card} htmlFor={props.card.internalId}><p style={{backgroundColor: props.card.colour}}>{cardName}</p><br/>Value: {props.card.value}<br/>{props.card.rent.map((r) => <p>{r}</p>)}</label>
             </span>);
         } else {
             let cardName = "N/A";
@@ -30,7 +33,7 @@ function Card(props) {
             }
             return (<span>
                 <input disabled={props.disabled} className={style.radioButton} type="radio" value={props.card} name="hand" id={props.card.internalId} onClick={() => {props.callback(props.card)}}/>
-                <label className={style.card} htmlFor={props.card.internalId}>{cardName}<br/>Type: {props.card.type}</label>
+                <label className={style.card} htmlFor={props.card.internalId}>{cardName}<br/>Type: {props.card.type}<br/>Value: {props.card.value}</label>
             </span>);
         }
     }
